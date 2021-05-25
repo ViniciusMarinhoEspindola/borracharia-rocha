761e10ea6413a127714211df45465f2c9bf2a962
root <root@ip-172-31-7-216.sa-east-1.compute.internal>
2021-05-26 04:09:58 +0800

    merge

--- resources/views/email/reset_password.blade.php
+++ resources/views/email/reset_password.blade.php
@@ -340,7 +340,7 @@
               <table role="presentation" border="0" cellpadding="0" cellspacing="0">
                 <tr>
                   <td class="content-block">
-                    <span class="apple-link">BMP</span>
+                    <span class="apple-link">Borracharia Rocha</span>
                   </td>
                 </tr>
               </table>
